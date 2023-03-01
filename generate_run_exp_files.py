a4739e47077935be540612a53d0ece18b3323a3c
Roi Naveiro <roinaveiro@Rois-MacBook-Air.local>
2023-03-01 16:07:47 +0100

    add exp files

---
+++ generate_run_exp_files.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+
+run = True
+problem = 'CON'
+learner = 'NGBlin'
+acqfun  = 'EI'
+search  = 'MH'
+epsilon = 0.0
+n_exp   = np.arange(1,9)
+
+header = '''#!/bin/bash
+#$ -q teano
+#$ -pe smp 5
+#$ -j yes
+#$ -cwd
+
+# Load anaconda malware environment
+conda activate base
+
+# Run the executable
+'''
+
+tail = '''
+
+# Deactivate anaconda environment
+conda deactivate
+'''
+
+text = []
+for i in n_exp:
+    current = (f"python -u run.py --problem {problem} "  
+               f"--learner {learner} --acqfun {acqfun} " 
+               f"--niters 500 --search {search} " 
+               f"--epsilon {epsilon} --nexp {i} " 
+               f"> exp_{i}_{problem}_{learner}_o{search}_af{acqfun}.out")
+    
+    current = header + current + tail
+    
+    with open(f'experiment{i}.sub', 'w') as f:
+        f.write(current)
+
+
+if run:
+    for i in n_exp:
+        command = f"qsub experiment{i}.sub"
+        os.system(command)
