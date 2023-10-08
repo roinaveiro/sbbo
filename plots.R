552882b4d37d5fb47146feee7147bcc454bfeb8f
Roi Naveiro <roinaveiro@Rois-MacBook-Air.local>
2023-10-08 16:46:34 +0200

    update

--- plots.R
+++ plots.R
@@ -9,49 +9,50 @@ height <- 5.79
 # OPT PLOTS
 ################
 
-# data <- read_csv("results/pRNA/pRNA_full_results_noBOCS.csv")
-data <- read_csv("results/CON/CON_full_results.csv")
+make_comparison_plot <- function(problem, label, iter_lim=500){
 
-pdata <- data %>% select(-current_vals) %>% 
-  mutate(Algorithm = algorithm) %>% 
-  filter( !(Algorithm %in% c("sbbo-NGBlin")) ) %>% 
-  group_by(iter, Algorithm) %>% 
-  summarise("Best Value" = mean(best_vals),  "Lower" = mean(best_vals) - 1*sd(best_vals),
-            "Upper" = mean(best_vals) + 1*sd(best_vals), "Std. Dev" = sd(best_vals))
+  data_path <- paste0("results/", problem, "/", problem, "_full_results.csv")
+  fig_path <- paste0("figs/", problem, ".png")
+  # data <- read_csv("results/pRNA/pRNA_full_results_noBOCS.csv")
+  data <- read_csv(data_path)
+  
+  pdata <- data %>% select(-current_vals) %>% 
+    mutate(Algorithm = algorithm) %>% 
+    filter( !(Algorithm %in% c("sbbo-NGBlin")) ) %>% 
+    filter(iter <= iter_lim) %>% 
+    group_by(iter, Algorithm) %>% 
+    summarise("Best Value" = mean(best_vals),  "Lower" = mean(best_vals) - 1*sd(best_vals),
+              "Upper" = mean(best_vals) + 1*sd(best_vals), "Std. Dev" = sd(best_vals))
+  
+  
+  pdata %>% 
+    ggplot(aes(x=iter, y=`Best Value`, color=Algorithm, fill=Algorithm)) + 
+    geom_line(size=0.5) +
+    geom_ribbon(aes(ymin=Lower,ymax=Upper), linetype = 0, alpha=0.1) +
+    #geom_errorbar(aes(ymin=Lower, ymax=Upper), size=0.5, alpha=0.25,
+    #              position=position_dodge(0.05)) +
+    # scale_color_viridis_d() + 
+    labs(title    = label,
+         #subtitle = TeX("|Q| = 20, |T| = 20, |X| = 20"),
+         x = "Iteration",
+         y = "Best Value") + 
+    theme_minimal() +
+    #theme(legend.position="bottom", legend.box = "horizontal") +
+    theme(axis.text.x=element_text(angle=-90, hjust=0, vjust=1)) +
+    theme(plot.title=element_text(size=15, hjust=0.5, face="bold", vjust=-1)) +
+    theme(plot.subtitle=element_text(size=12, hjust=0.5, vjust=-1)) +
+    theme(text = element_text(size=12)) +
+    theme(legend.title = element_text(face = "bold")) 
+  
+  
+  ggsave(filename = fig_path, 
+         device = "png", 
+         dpi = dpi, width = width, height = height)
+}
 
-branded_colors <- c(
-  "RS" = "#00798c",
-  "SA" = "#d1495b",
-  "sbbo-BOCS" = "#edae49",
-  "sbbo-GPr" = "#66a182",
-  "sbbo-NGBdec" = "#2e4057",
-  "sbbo-NGBlinCV" = "#8d96a3"
-)
-
-pdata %>% filter(iter %% 5 == 0) %>% 
-  ggplot(aes(x=iter, y=`Best Value`, color=Algorithm, fill=Algorithm)) + 
-  geom_line(size=0.5) +
-  geom_ribbon(aes(ymin=Lower,ymax=Upper), linetype = 0, alpha=0.1) +
-  #geom_errorbar(aes(ymin=Lower, ymax=Upper), size=0.5, alpha=0.25,
-  #              position=position_dodge(0.05)) +
-  # scale_color_viridis_d() + 
-  labs(title    = "Contamination Problem",
-       #subtitle = TeX("|Q| = 20, |T| = 20, |X| = 20"),
-       x = "Iteration",
-       y = "Best Value") + 
-  theme_minimal() +
-  #theme(legend.position="bottom", legend.box = "horizontal") +
-  theme(axis.text.x=element_text(angle=-90, hjust=0, vjust=1)) +
-  theme(plot.title=element_text(size=15, hjust=0.5, face="bold", vjust=-1)) +
-  theme(plot.subtitle=element_text(size=12, hjust=0.5, vjust=-1)) +
-  theme(text = element_text(size=12)) +
-  theme(legend.title = element_text(face = "bold")) 
-
-
-
-ggsave(filename = "figs/CON.png", 
-       device = "png", 
-       dpi = dpi, width = width, height = height)
+make_comparison_plot("CON", "Contamination Problem")
+make_comparison_plot("pRNA", "MFE RNA Design Problem", iter_lim=300)
+make_comparison_plot("BQP", "Binary Quadratic Problem")
 
 ################
 # ACC PLOTS
--- preprocess_results.R
+++ preprocess_results.R
@@ -1,22 +1,29 @@
 library(tidyverse)
 source("utils.R")
 
-
 results_path <- "/Users/roinaveiro/Library/CloudStorage/OneDrive-CUNEF/CUNEF/research/sbbo/results/"
-problem <- "CON"
 
-#models <- c("RS", "SA", "MH/BOCS", "MH/BNN", "MH/GPr", "MH/NGBdec", 
-#             "MH/NGBlinCV")
-#all_labels <- c("RS", "SA", "sbbo-BOCS", "sbbo-BNN", "sbbo-GPr", "sbbo-NGBdec", 
-#                "sbbo-NGBlinCV")
+preprocess_results <- function(problem){
+  
+  #models <- c("RS", "SA", "MH/BOCS", "MH/BNN", "MH/GPr", "MH/NGBdec", 
+  #             "MH/NGBlinCV")
+  #all_labels <- c("RS", "SA", "sbbo-BOCS", "sbbo-BNN", "sbbo-GPr", "sbbo-NGBdec", 
+  #                "sbbo-NGBlinCV")
+  
+  models <- c("RS", "SA", "MH/BOCS", "MH/GPr", "MH/NGBdec", 
+              "MH/NGBlinCV", "MH/BNN")
+  all_labels <- c("RS", "SA", "sbbo-BLr", "sbbo-GPr", "sbbo-NGBdec", 
+                  "sbbo-NGBlinCV", "sbbo-BNN")
+  
+  
+  all_models <- paste0(results_path, problem, "/", models)
+  full_df <- process_models(all_models, all_labels)
+  
+  write.csv(full_df, 
+            paste0("results/", problem, "/", problem, "_full_results.csv"), 
+            row.names = F)
+  
+}
 
-models <- c("RS", "SA", "MH/BOCS", "MH/GPr", "MH/NGBdec", 
-             "MH/NGBlinCV")
-all_labels <- c("RS", "SA", "sbbo-BLr", "sbbo-GPr", "sbbo-NGBdec", 
-                "sbbo-NGBlinCV")
-
-
-all_models <- paste0(results_path, problem, "/", models)
-full_df <- process_models(all_models, all_labels)
-
-write.csv(full_df, "results/CON/CON_full_results.csv", row.names = F)
+preprocess_results("CON")
+preprocess_results("pRNA")
