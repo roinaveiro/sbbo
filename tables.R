7fa8d610b99249bb65107e0b0776f6b6a90d0ed8
Roi Naveiro <roinaveiro@Rois-MacBook-Air.local>
2023-11-18 10:47:14 +0100

    update

--- tables.R
+++ tables.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library(tidyverse)
 library(latex2exp)
 library(kableExtra)
@@ -75,3 +76,52 @@ tex_out <- pdata %>%
   kbl(caption = label, format = "latex", booktabs=T, escape = F) %>% kable_styling(latex_options = c("hold_position"))
 
 
+=======
+library(tidyverse)
+library(latex2exp)
+library(kableExtra)
+
+################
+# Tables
+################
+
+make_table <- function(problem, label, iter_lim=499){
+
+  data_path <- paste0("results/", problem, "/", problem, "_full_results.csv")
+  fig_path <- paste0("figs/", problem, ".png")
+  # data <- read_csv("results/pRNA/pRNA_full_results_noBOCS.csv")
+  data <- read_csv(data_path)
+  
+  pdata <- data %>% select(-current_vals) %>% 
+    mutate(Algorithm = algorithm) %>% 
+    filter( !(Algorithm %in% c("sbbo-NGBlin")) ) %>% 
+    filter(iter == iter_lim) %>% 
+    group_by(iter, Algorithm) %>% 
+    summarise("Best Value" = mean(best_vals),  "Lower" = mean(best_vals) - 1*sd(best_vals),
+              "Upper" = mean(best_vals) + 1*sd(best_vals), "Std. Dev" = sd(best_vals)) %>% 
+    ungroup() %>% 
+    select(Algorithm, `Best Value`, `Std. Dev`)
+  
+  tex_out <- pdata %>% 
+    mutate(
+      `Best Value` = paste("$", as.character(round(`Best Value`,2)), "\\pm", round(`Std. Dev`, 2), "$"),
+    ) %>%
+    select(Algorithm, `Best Value` ) %>% 
+    kbl(caption = label, format = "latex", booktabs=T, escape = F) %>% kable_styling(latex_options = c("hold_position"))
+    
+  return(tex_out)
+}
+
+iter_lim <-  100
+label <- paste("Cap")
+make_table("BQP", label, iter_lim = iter_lim)
+
+iter_lim <-  499
+make_table("CON", label, iter_lim = iter_lim)
+
+iter_lim <-  299
+make_table("pRNA", label, iter_lim = iter_lim)
+
+
+
+>>>>>>> dd9bde0 (update)
